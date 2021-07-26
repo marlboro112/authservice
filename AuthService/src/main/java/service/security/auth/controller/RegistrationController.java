a9a438e05a1cb629fb3c5c9881eccf87d1405aab
rashad <rashad@10.0.2.15>
2021-07-26 18:15:51 +0400

    test

--- AuthService/src/main/java/service/security/auth/controller/RegistrationController.java
+++ AuthService/src/main/java/service/security/auth/controller/RegistrationController.java
@@ -5,6 +5,7 @@ import java.util.Locale;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,5 +35,14 @@ public class RegistrationController {
 	}
 	
 	
+	@PostMapping(path = "/{id}", consumes = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE }, produces = {
+			MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
+	public String emailVerification (@PathVariable String id ) {
+		
+		
+		
+		return "ok";
+		
+	}
 
 }
